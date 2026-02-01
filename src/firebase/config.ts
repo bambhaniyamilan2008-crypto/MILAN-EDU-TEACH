948b01e4df8108d9d403b66baf38e8633a9963c4
Milan Bambhaniya <bambhaniyamilan.2008@gmail.com>
2026-02-01 15:02:22 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+// config.ts
 export const firebaseConfig = {
-  "projectId": "studio-8186080283-f4574",
-  "appId": "1:31466290334:web:027b890f614e7aceb68f8f",
-  "apiKey": "AIzaSyAWbOFNGqZ44QupcKPeX7yGsF6zPvp1PVk",
-  "authDomain": "studio-8186080283-f4574.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "31466290334"
+  projectId: "studio-8186080283-f4574",
+  appId: "1:31466290334:web:027b890f614e7aceb68f8f",
+  apiKey: "AIzaSyAWbOFNGqZ44QupcKPeX7yGsF6zPvp1PVk",
+  authDomain: "studio-8186080283-f4574.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "31466290334"
 };
